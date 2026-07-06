777918eac50b284d421a80aee263581fbae5b6cf
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-07 04:50:44 +0530

    Updated Integer.MIN_VALUE

--- Array/LargeNum.java
+++ Array/LargeNum.java
@@ -1,7 +1,7 @@
 class Large{
     public static void main(String[] args) {
         int arr[] = new int[]{1,2,4,5,6,7};
-        int large = arr[0];
+        int large = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++) {
             if (arr[i] > large) {
                 large = arr[i];
