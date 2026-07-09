8630302cedc3a9ff0ffdedc58d4d4b253e6bc2f4
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-09 07:20:28 +0530

    Subarray in Java

---
+++ Array/Subarray.java
@@ -0,0 +1,16 @@
+class Subarray{
+    public static void main(String[] args) {
+        int arr[] = new int[]{1,2,3,4,5};
+        for(int i = 0;i<arr.length;i++){
+            for(int j = 0;j<arr.length;j++){
+                for(int k = i;k<=j;k++){
+                    System.out.print(arr[k]+" ");
+                }
+                System.out.println();
+                
+            }
+            System.out.println();
+        }
+        
+    }
+}
