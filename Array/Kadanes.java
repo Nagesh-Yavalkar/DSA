388100f4f48e3a7df0b802f69cc80c89012854b6
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-12 23:34:05 +0530

    Kadanes algorithm for maximum sum of Subarray

---
+++ Array/Kadanes.java
@@ -0,0 +1,15 @@
+class Kadanes{
+    public static void main(String[] args) {
+        int arr[] = new int[]{-1,-2,3,-4,5};
+        int cs = 0;
+        int ms = Integer.MIN_VALUE;
+        for(int i = 0;i<arr.length;i++){
+            cs = cs+arr[i];
+            if(cs<0){
+                cs = 0;
+            }
+            ms = Math.max(cs,ms);
+        }
+        System.out.println("Our maximum sum of Subarray is : "+ms);
+    }
+}
