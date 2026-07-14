8270799fa7bbec08b554c4144ea210ef9ff50a05
Nagesh-Yavalkar <nageshyavalkar@gmail.com>
2026-07-14 23:40:33 +0530

    Updated comments

--- Array/Kadanes.java
+++ Array/Kadanes.java
@@ -13,3 +13,6 @@ class Kadanes{
         System.out.println("Our maximum sum of Subarray is : "+ms);
     }
 }
+
+// Approach of Kadanes algorithm is if you adding array indexwise then your sum is less than zero
+//then your maximum sum is equal to zero and if your additon is greater than zero then it add it maximum variable
